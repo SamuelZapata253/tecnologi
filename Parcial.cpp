d4b8a5bdd3261becaa1b35a0335a8c3928dc3a39
SamuelZapata253 <77840674+SamuelZapata253@users.noreply.github.com>
2021-02-20 13:34:08 -0500

    Add files via upload

---
+++ Parcial.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+#include<conio.h>
+#include <windows.h> 
+#include <time.h> 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string>
+
+main(){
+	while
+}
